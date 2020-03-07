e3b14c97b97a7ff41468e5a59dbbd64f1467d3d0
pantomas <sych.tomasz@gmail.com>
2020-03-07 13:32:47 +0100

    Updates

---
+++ precache-manifest.83a3eac0667a4e890b076c6f361bb566.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "150e9e4fd302901ae5a2c507d4b52a82",
+    "url": "/apka2/index.html"
+  },
+  {
+    "revision": "03b4e4fcaf5985b97027",
+    "url": "/apka2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c77e1b7b6b4c95136b6e",
+    "url": "/apka2/static/js/2.6bec0263.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/apka2/static/js/2.6bec0263.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03b4e4fcaf5985b97027",
+    "url": "/apka2/static/js/main.676aadfd.chunk.js"
+  },
+  {
+    "revision": "846e1e5f4eef1d5b9aca",
+    "url": "/apka2/static/js/runtime-main.18507d2d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/apka2/static/media/logo.5d5d9eef.svg"
+  }
+]);
